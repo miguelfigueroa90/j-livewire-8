b7a96e676b1cada4346d7f5e67953669b56e5cd7
Miguel Figueroa <miguelfigueroa90@gmail.com>
2021-01-29 09:23:26 -0400

    Adding course detail lessons list

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -7,7 +7,17 @@
                 <li class="font-medium text-sm text-gray-400 uppercase mb-4">
                     Content
                 </li>
-                ---
+                @foreach ($course->posts as $post)
+                    <li class="flex items-center text-gray-600 mt-2">
+                        {{ $post->name }}
+                        @if ($post->free)
+                            <span
+                                class="text-xs text-gray-500 font-semibold bg-gray-300 px-2 rounded-full ml-auto">
+                                Free
+                            </span>
+                        @endif
+                    </li>
+                @endforeach
             </ul>
         </div>
         <div class="text-gray-700 col-span-2">
@@ -15,17 +25,12 @@
             <h2 class="text-4xl">{{ $course->name }}</h2>
             <p>{{ $course->description }}</p>
             <div class="flex mt-3">
-                <img
-                    src="{{ $course->user->avatar }}"
-                    class="h-10 w-10 rounded-full mr-2"
-                >
+                <img src="{{ $course->user->avatar }}" class="h-10 w-10 rounded-full mr-2">
                 <div>
-                    <p
-                        class="text-gray-500 text-sm">
+                    <p class="text-gray-500 text-sm">
                         {{ $course->user->name }}
                     </p>
-                    <p
-                        class="text-gray-400 text-xs">
+                    <p class="text-gray-400 text-xs">
                         {{ $course->created_at->diffForHumans() }}
                     </p>
                 </div>
